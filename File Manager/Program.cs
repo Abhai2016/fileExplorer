34a1b95cf4988fdf7d2c8edcf3ab47cfc382bfa3
Abhai2016 <abhai2013@gmail.com>
2020-03-12 11:53:46 +0200

    First commit. Created the project

---
+++ File Manager/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace File_Manager
+{
+    class Program
+    {
+        private const string title = "File Manager";
+
+
+
+        static void Main(string[] args)
+        {
+            WriteCenterColorText(title, ConsoleColor.Blue);
+        }
+
+
+        private static void WriteCenterColorText(String text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(new string(' ', (Console.WindowWidth - text.Length) / 2)); 
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+    }
+}
